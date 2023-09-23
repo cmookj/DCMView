e306bdc00f4c47d79116fc82fbd09bd6db17de06
Changmook Chun <cmookj@gmail.com>
2023-09-23 10:53:08 -0700

    Initial commit

---
+++ CppTest/main.cpp
@@ -0,0 +1,36 @@
+//
+//  main.cpp
+//  CppTest
+//
+//  Created by Changmook Chun on 9/22/23.
+//
+
+#include "dcmtk/dcmimgle/dcmimage.h"
+#include "dcmtk/dcmimgle/dipixel.h"
+#include "dcmtk/dcmimgle/diimage.h"
+#include "dcmtk/dcmimgle/dimo1img.h"
+#include "dcmtk/dcmimgle/dimo2img.h"
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    if (DicomImage* image = new DicomImage("test.dcm")) {
+        if (image->getStatus() == EIS_Normal) {
+            if (image->isMonochrome()) {
+                image->setMinMaxWindow();
+                Uint8 *pixelData = (Uint8 *)(image->getOutputData(8 /* bits */));
+                if (pixelData != NULL) {
+                    /* do something useful with the pixel data */
+                }
+            }
+            
+        } else {
+            cerr << "Error: cannot load DICOM image (" << DicomImage::getString(image->getStatus()) << ")\n";
+        }
+        
+        delete image;
+    }
+    
+    return 0;
+}
